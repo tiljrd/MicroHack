f668ca8924c745ddc9c3ff855362b21c0125f182
tiljrd <45370031+tiljrd@users.noreply.github.com>
2019-11-03 02:02:59 +0000

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -22,12 +22,16 @@ public class MainActivity extends AppCompatActivity {
 
     //Database reference
     private DatabaseReference databaseReference;
+    
+    //TextView which shows the state of the door
     TextView textView;
 
+    //Timer for checking the status of the door every three seconds
     private Timer mTimer1;
     private TimerTask mTt1;
     private Handler mTimerHandler = new Handler();
 
+    //not yet completely implemented but needed for opening the door with the phone
     String pressed = "";
 
     @Override
@@ -35,20 +39,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initialise the Text view and the database reference
         textView = findViewById(R.id.textView);
         databaseReference = FirebaseDatabase.getInstance().getReference("lock");
 
+        //check the door state by retrieving it from the database
         getDoorState();
+        
+        //start the timer to check for the state every three seconds
         startTimer();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        
+        //stop the timer when the activity is destroyed
         stopTimer();
     }
 
+    //get the door state from the database
+    //0 or 1 for closed or open respectively
     private void getDoorState() {
+        
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -64,6 +77,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
+            //when the connection to the database fails
+            //still room for improvement here
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 Log.i("Error", databaseError.toString());
@@ -71,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //stop the timer which retrieves the state of the database
     private void stopTimer(){
         if(mTimer1 != null){
             mTimer1.cancel();
@@ -78,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //start the timer to retrieve the state of the door from the database every three seconds
     private void startTimer(){
         mTimer1 = new Timer();
         mTt1 = new TimerTask() {
@@ -93,11 +110,17 @@ public class MainActivity extends AppCompatActivity {
         mTimer1.schedule(mTt1, 1, 3000);
     }
 
+    //using Tags find out what passcode was pressed by the user
+    //still not fully implemented (opening the door with the phone)
     public void pressed(View view) {
+        
         Button button = (Button) view;
 
+        //upload the confiirmed code into the database
         if(button.getTag().toString().equals("C")) {
             databaseReference.child("pressed").setValue(Long.valueOf(pressed));
+            
+        //add the characters that the user is pressing together to a passcode
         } else {
             pressed += button.getTag().toString();
         }
